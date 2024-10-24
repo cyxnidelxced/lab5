f4fc73e2b0909ab3fa4244b42ac193fd49ccf352
cyxnidelxced <jnguyen257@insite.4cd.edu>
2024-10-23 19:25:56 -0700

    background img, col2 edit, popups

--- js/main.js
+++ js/main.js
@@ -58,8 +58,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
     var myCues = [
         { seconds: 2, callback: func1 },
-        { seconds: 9, callback: func2 },
-        { seconds: 15, callback: func3 }
+        { seconds: 10, callback: func2 },
+        { seconds: 20, callback: func3 },
+        { seconds: 49, callback: func4 }
     ];
 
     //this activates the cuepoints module.
@@ -82,13 +83,33 @@ document.addEventListener("DOMContentLoaded", (e) => {
 //You can code up whatever behavior you need in your own callbacks
 //feel free to rename the functions to be more descriptive of what they do.
 function func1() {
-
+    document.querySelector("#vid").style = "outline : 3px solid #5C64E0";
 }
 
 function func2() {
-    
+    let pop = document.querySelector(".pop");
+    pop.innerHTML = "<p>Check out his wiki page</p>";
+    pop.classList.toggle("hide");
+    setTimeout(() => {
+        document.querySelector(".pop").classList.toggle("hide");
+    }, 2000);
+    document.querySelector("#vid").style = "outline: 0px solid #5C64E0";
+    document.querySelector("#web").src =
+        "https://en.wikipedia.org/wiki/Hayao_Miyazaki";
 }
 
 function func3() {
+    const pop = document.querySelector(".pop");
+    pop.innerHTML = "<p>Awards won</p>";
+    pop.classList.toggle("hide");
+    setTimeout(() => {
+        document.querySelector(".pop").classList.toggle("hide");
+    }, 2000);
+    document.querySelector("#vid").style = "outline: 0px solid #5C64E0";
+    document.querySelector("#web").src =
+        "https://ghibli.fandom.com/wiki/Howl%27s_Moving_Castle/Accolade";
+}
 
+function func4() {
+    document.querySelector("#web").src = "summary.html";
 }
